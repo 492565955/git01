3fbd66e3b7685b283029aa9f3bca0d405e1417ce
ant <492565955@qq.com>
2019-05-29 20:14:51 +0800

    this is two update

--- src/main/java/cn/juechuang/contrller/test01.java
+++ src/main/java/cn/juechuang/contrller/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("我是初始值");
         System.out.println("我是第一次改变");
+        System.out.println("这是第二次改变");
     }
 }
