031090ca0f72755a448333ecc85aea9ce402483d
ant <492565955@qq.com>
2019-05-29 20:13:36 +0800

    this is frist update

--- src/main/java/cn/juechuang/contrller/test01.java
+++ src/main/java/cn/juechuang/contrller/test01.java
@@ -3,5 +3,6 @@ package cn.juechuang.contrller;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("我是初始值");
+        System.out.println("我是第一次改变");
     }
 }
